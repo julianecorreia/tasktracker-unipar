9ca68fa5b590922464287a2b9e945d1c89595328
juFrabel <15932624+juFrabel@users.noreply.github.com>
2025-09-08 19:28:45 -0300

    first commit

---
+++ src/app/header/header.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+//padrão de projeto DECORATOR
+//https://refactoring.guru/pt-br/design-patterns/decorator
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+
+}
